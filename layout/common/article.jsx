bada0e45bc1694b9cb5c0fdbb3551ad9e747ba03
Hyiker <hyikerhu0212@gmail.com>
2023-06-28 14:58:53 +0800

    add prefix for title1

--- layout/common/article.jsx
+++ layout/common/article.jsx
@@ -85,7 +85,7 @@ module.exports = class extends Component {
                     </div> : null}
                     {/* Title */}
                     {page.title !== '' && index ? <p class="title is-3 is-size-4-mobile"><a class="link-muted" href={url_for(page.link || page.path)}>{page.title}</a></p> : null}
-                    {page.title !== '' && !index ? <h1 class="title is-3 is-size-4-mobile">{page.title}</h1> : null}
+                    {page.title !== '' && !index ? <h1 class="title title-prefix is-3 is-size-4-mobile">{page.title}</h1> : null}
                     {/* Content/Excerpt */}
                     <div class="content" dangerouslySetInnerHTML={{ __html: index && page.excerpt ? page.excerpt : page.content }}></div>
                     {/* Licensing block */}
